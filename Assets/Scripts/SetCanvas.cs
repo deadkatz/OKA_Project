9367e02dccfa3c999390143a5f3e8eeb609b75d9
Jakub Cieślik <deadkatz@tlen.pl>
2015-07-28 12:00:48 +0200

    dodanoskrypt od jasnego koloru na iknach

---
+++ Assets/Scripts/SetCanvas.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetCanvas : MonoBehaviour {
+
+	public bool isEnabled;
+
+	// Use this for initialization
+	void Start () {
+		if (isEnabled == true) {
+			GetComponent<Canvas> ().sortingOrder = 1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
