bcd6d9958ec2c470e0ff2c4f8604d94a218167cb
Jakub Cieślik <deadkatz@tlen.pl>
2015-12-07 14:55:49 +0100

    Poprawki_do_skalowania

---
+++ Assets/Scripts/HiResScreenshot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiResScreenshot : MonoBehaviour {
+
+
+	
+	public void email_send()
+	{
+		Application.CaptureScreenshot("Screenshot.png");
+	}
+}
