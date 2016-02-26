dbf23d4320912969992674ee0a78d4b2e317a6a6
Jakub Cieślik <deadkatz@tlen.pl>
2016-02-26 17:01:18 +0100

    popraweczki sreczki

---
+++ Assets/Scripts/ZRenderQueue.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZRenderQueue : MonoBehaviour {
+	
+	// Use this for initialization
+		protected void Start() {
+
+		transform.GetComponent<MeshRenderer> ().sharedMaterial.renderQueue = 3001;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
