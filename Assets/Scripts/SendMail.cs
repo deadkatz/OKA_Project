83fb4a70b3601d04c86032780e355a51412ae8dd
Jakub Cieślik <deadkatz@tlen.pl>
2016-03-04 15:18:18 +0100

    minor poprawki

--- Assets/Scripts/SendMail.cs
+++ Assets/Scripts/SendMail.cs
@@ -59,55 +59,26 @@ public class SendMail : MonoBehaviour {
 	
 	public void SendMailTo() {
 		ThreadPool.QueueUserWorkItem(x => ThreadMail());
-		//		ThreadMail ();
-		
-		
-		//		SmtpClient smtpClient = new SmtpClient();
-		//		NetworkCredential basicCredential = 
-		//			new NetworkCredential("kuba@3monkeys.pl", "cieslIK@2015"); 
-		//		MailMessage message = new MailMessage(); 
-		//		MailAddress fromAddress = new MailAddress("kuba@3monkeys.pl"); 
-		//		
-		//		smtpClient.Host = "smtp.3monkeys.pl";
-		//		smtpClient.UseDefaultCredentials = false;
-		//		smtpClient.Credentials = basicCredential;
-		//		
-		//		message.From = fromAddress;
-		//		message.Subject = "your subject";
-		//		//Set IsBodyHtml to true means you can send HTML email.
-		//		message.IsBodyHtml = true;
-		//		message.Body = "<h1>your message body</h1>";
-		//		message.To.Add("karcisze@gmail.com"); 
-		//		
-		//		try
-		//		{
-		//			smtpClient.Send(message);
-		//		}
-		//		catch(Exception ex)
-		//		{
-		//			//Error, could not send the message
-		//			Debug.Log (ex.Message);
-		//		}
+				ThreadMail ();
 		
 	}
 	void ThreadMail(){
 		email = nameInputField.text;
 		Debug.Log (email);
 		
-		MailMessage mail = new MailMessage("app@oka-office.eu", email);
+		MailMessage mail = new MailMessage("kuba@3monkeys.pl", email);
 		Attachment a = new Attachment (path, MediaTypeNames.Application.Octet);
 		mail.Attachments.Add (a);
 		SmtpClient client = new SmtpClient();
-		client.Host = "w0062da6.kasserver.com";
-		client.Port = 25;
+		client.Host = "smtp.3monkeys.pl";
+		client.Port = 587;
 		client.DeliveryMethod = SmtpDeliveryMethod.Network;
 		client.UseDefaultCredentials = false;
-		client.Credentials = new System.Net.NetworkCredential ("m037d95d", "3mVEVGT7wKwKDb5X") as ICredentialsByHost;
+		client.Credentials = new System.Net.NetworkCredential ("kuba@3monkeys.pl", "cieslIK_15") as ICredentialsByHost;
 		client.EnableSsl = true;
 		ServicePointManager.ServerCertificateValidationCallback =
 			delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
 		{ return true; };
-		//client.Host = "smtp.3monkeys.pl";
 		mail.Subject = "OKA Configuration";
 		mail.Body = "";
 		client.Send(mail);
